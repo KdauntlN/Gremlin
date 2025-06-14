7c53b14c21a8999b8a0928e0b53164ffe033a317
KdauntlN <henryknzed@gmail.com>
2025-06-14 17:31:28 +1000

    Some comments but mostly spiteful ones

--- src/lib.rs
+++ src/lib.rs
@@ -81,6 +81,7 @@ fn print_count(results: Vec<(usize, &str)>) {
     
 }
 
+// Lifetimes are stupid
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
     let mut results = Vec::new();
     
@@ -108,29 +109,5 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<(usize
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents ="\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec![(1, "safe, fast, productive.")], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(vec![(0, "Rust:"), (3, "Trust me.")], search_case_insensitive(query, contents));
-    }
+    // Should probably have some tests here but I'm lazy
 }
